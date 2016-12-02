22ae49070b7e45d1d3130544f8ce4d5c49143f84
Haye Böhm <haye.bohm@topicus.nl>
2016-12-02 19:56:01 +0100

    Bugfixes

--- src/deriha/ChessTest.java
+++ src/deriha/ChessTest.java
@@ -9,6 +9,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -21,74 +22,83 @@ import org.openqa.selenium.chrome.ChromeDriver;
 /**
  * @author haye
  */
-public class ChessTest {
+public class ChessTest
+{
 	private WebDriver driver;
 
-	private String[] whiteChessPieces = {".pawn.white", ".king.white", ".queen.white", ".knight.white",
-			".rook.white",
-			".bishop.white"};
+	private String[] whiteChessPieces = { ".pawn.white", ".king.white", ".queen.white", ".knight.white",
+		".rook.white",
+		".bishop.white" };
 
-	private String[] blackChessPieces = {".pawn.black", ".king.black", ".queen.black", ".knight.black",
-			".rook.black",
-			".bishop.black"};
+	private String[] blackChessPieces = { ".pawn.black", ".king.black", ".queen.black", ".knight.black",
+		".rook.black",
+		".bishop.black" };
 
-	private String[] allPieces = {" .pawn", " .king", " .queen", " .knight", " .rook", ".bishop"};
+	private String[] allPieces = { " .pawn", " .king", " .queen", " .knight", " .rook", ".bishop" };
 
 	private Random random = new Random();
 
-	public ChessTest() {
+	public ChessTest()
+	{
 		driver = new ChromeDriver();
 	}
 
 	/**
-	 * Move a random piece to one of its random possible destinations TODO: Fix taken a different piece
+	 * Move a random piece to one of its random possible destinations
 	 */
-	private void move() throws InterruptedException {
-
+	private void move() throws InterruptedException
+	{
 		List<WebElement> elements = Arrays.stream(whiteChessPieces).map(s -> driver.findElements(By.cssSelector(s)))
-				.flatMap(List::stream)
-				.collect(Collectors.toList());
+			.flatMap(List::stream)
+			.collect(Collectors.toList());
+		Collections.shuffle(elements);
 
-		List<WebElement> targets;
-		int checked = 0;
-		do {
-			WebElement piece = elements.get(Math.abs(random.nextInt()) % elements.size());
+		List<WebElement> targets = null;
+		for (WebElement piece : elements)
+		{
 			System.out.println("Select piece " + piece.getAttribute("class"));
 			piece.click();
 			Thread.sleep(200);
 			targets = driver.findElements(By.cssSelector(".move-dest"));
-			checked++;
+			if (targets.size() > 0)
+			{
+				break;
+			}
 		}
-		while (targets.isEmpty() && checked <= elements.size());
-		Thread.sleep(500);
 
-		List<WebElement> blackPieces = Arrays.stream(blackChessPieces).map(s -> driver.findElements(By.cssSelector(s)))
+		if (targets != null)
+		{
+			List<WebElement> blackPieces = Arrays.stream(blackChessPieces).map(s -> driver.findElements(By.cssSelector(s)))
 				.flatMap(List::stream)
 				.collect(Collectors.toList());
 
-		WebElement toClick = toDestination(targets.get(Math.abs(random.nextInt()) % targets.size()), blackPieces);
+			WebElement toClick = toDestination(targets.get(Math.abs(random.nextInt()) % targets.size()), blackPieces);
 
-		System.out.println("Move to " + toClick.getAttribute("style"));
-		Thread.sleep(100);
-		toClick.click();
+			System.out.println("Move to " + toClick.getAttribute("style"));
+			toClick.click();
+		}
+		else
+		{
+			System.err.println("Could not find a possible move");
+		}
+
 	}
 
 	/**
 	 * Returns the element to be clicked on: Another piece if it is has the same location as the target, else the target.
 	 */
-	private WebElement toDestination(WebElement target, List<WebElement> elements) {
-		WebElement t = target;
-		for (WebElement e : elements) {
-			if (e.getAttribute("style").equals(target.getAttribute("style"))) {
-				t = e;
-			}
-		}
-		return t;
-		//return elements.stream().filter(webElement -> webElement.getAttribute("style").equals(target.getAttribute("style"))).findFirst().orElse(target);
+	private WebElement toDestination(WebElement target, List<WebElement> elements)
+	{
+		return elements.stream()
+			.filter(webElement -> webElement.getAttribute("style").equals(target.getAttribute("style")))
+			.findFirst()
+			.orElse(target);
 	}
 
-	private void start(int port) {
-		try {
+	private void start(int port)
+	{
+		try
+		{
 			ServerSocket servsock = new ServerSocket(port);
 			Socket sock = servsock.accept();
 			InputStream inStream = sock.getInputStream();
@@ -96,67 +106,72 @@ public class ChessTest {
 			OutputStream outStream = sock.getOutputStream();
 			PrintWriter sockout = new PrintWriter(new OutputStreamWriter(outStream));
 
-			String received;
 			boolean end = false;
-			System.out.println("Started listening for command..");
+			System.out.println("Started listening for commands..");
+
+			// Send the board config so that the model knows the start config
 			sendBoardCode(sockout);
-			while (!end) {
-				received = sockin.readLine();
+			while (!end)
+			{
+				String received = sockin.readLine();
 				System.out.println("Received command \"" + received + "\"");
 				received = received.trim();
 
-				switch (received) {
-					case "Move":
-						move();
-						Thread.sleep(1000);
-						sendBoardCode(sockout);
-						break;
-					case "Back":
-						back();
-						Thread.sleep(1000);
-						sendBoardCode(sockout);
-						break;
-					case "BackStep":
-						backStep();
-						backStep();
-						Thread.sleep(2400);
-						sendBoardCode(sockout);
-						break;
-					case "Forward":
-						forward();
-						Thread.sleep(2400);
-						sendBoardCode(sockout);
-						break;
-					case "ForwardStep":
-						forwardStep();
-						forwardStep();
-						Thread.sleep(2400);
-						sendBoardCode(sockout);
-						break;
-					default:
-						System.err.println("Received unknown command \"" + received + "\"");
+				switch (received)
+				{
+				case "Move":
+					move();
+					Thread.sleep(500);
+					break;
+				case "Back":
+					back();
+					Thread.sleep(500);
+					break;
+				case "BackStep":
+					backStep();
+					backStep();
+					Thread.sleep(500);
+					break;
+				case "Forward":
+					forward();
+					Thread.sleep(500);
+					break;
+				case "ForwardStep":
+					forwardStep();
+					forwardStep();
+					Thread.sleep(500);
+					break;
+				default:
+					System.err.println("Received unknown command \"" + received + "\"");
 				}
+				sendBoardCode(sockout);
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	private String getBoardState() {
+	private String getBoardState() throws InterruptedException
+	{
+		Thread.sleep(200);
 		List<WebElement> el = Arrays.stream(allPieces).map(s -> driver.findElements(By.cssSelector(s)))
-				.flatMap(List::stream)
-				.collect(Collectors.toList());
+			.flatMap(List::stream)
+			.collect(Collectors.toList());
 
 		StringBuilder sb = new StringBuilder();
 		el.forEach(e -> sb.append(e.getAttribute("class")).append(" ").append(e.getAttribute("style")));
 		return sb.toString();
 	}
 
-	private void end() {
+	private void end()
+	{
 		driver.close();
 	}
 
-	private void setup() throws InterruptedException {
+	private void setup() throws InterruptedException
+	{
 		driver.get("https://en.lichess.org/");
 
 		Thread.sleep(1000);
@@ -172,10 +187,12 @@ public class ChessTest {
 		Thread.sleep(500);
 	}
 
-	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	public static void main(String[] args) throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.mac");
 
-		if (args.length != 1) {
+		if (args.length != 1)
+		{
 			System.err.println("Required port number as first argument");
 			return;
 		}
@@ -188,26 +205,31 @@ public class ChessTest {
 		ct.end();
 	}
 
-	private void sendBoardCode(PrintWriter sockout) {
+	private void sendBoardCode(PrintWriter sockout) throws InterruptedException
+	{
 		int boardCode = Math.abs(getBoardState().hashCode());
 		System.out.println("Board code " + boardCode);
 		sockout.print(boardCode + "\n");
 		sockout.flush();
 	}
 
-	private void back() {
+	private void back()
+	{
 		driver.findElement(By.xpath("//*[@id=\"lichess\"]/div/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[1]/nav/button[1]")).click();
 	}
 
-	private void forward() {
+	private void forward()
+	{
 		driver.findElement(By.xpath("//*[@id=\"lichess\"]/div/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[1]/nav/button[4]")).click();
 	}
 
-	private void backStep() {
+	private void backStep()
+	{
 		driver.findElement(By.xpath("//*[@id=\"lichess\"]/div/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[1]/nav/button[2]")).click();
 	}
 
-	private void forwardStep() {
+	private void forwardStep()
+	{
 		driver.findElement(By.xpath("//*[@id=\"lichess\"]/div/div[1]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[1]/nav/button[3]")).click();
 	}
 }
